ef0254ab06e40b986e58a8725b1fbaa53da721b0
prafullapal <prafullpal18@gmail.com>
2024-01-17 19:23:21 +0530

    Zomato Clone Setup TailwindCss

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text className="text-red-500">This is a zomato clone</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
