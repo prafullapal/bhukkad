47b141d4ff319457a5b06e8de77fb22d69f75706
prafullapal <prafullpal18@gmail.com>
2024-01-18 15:25:17 +0530

    Sanity Installed and Schema Updated

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'y24581s4',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/index.ts
@@ -0,0 +1,5 @@
+import category from './category'
+import restaurant from './restaurant'
+import thedish from './dish'
+import featured from './featured'
+export const schemaTypes = [category, restaurant, thedish, featured]
